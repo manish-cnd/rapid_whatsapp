521adbf6249d636b8e86171f11b925082fa446e6
manish-cnd <manishkumartiwari735@gmail.com>
2025-03-29 19:57:36 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
             className="btn btn-outline-light"
             target="_blank"
             rel="noreferrer"
-            href="https://github.com/Vishesh-Pandey/rapid-whatsapp"
+            href="https://github.com/manish-cnd/rapid_whatsapp"
           >
             Code
           </a>
